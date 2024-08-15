0f228c4a2ad2dc212f33a19b8ea14a066d9b4f3f
DanielSohrabi <daniel.sohrabi.2005@gmail.com>
2024-08-15 18:38:59 -0400

    Version 1.0

---
+++ scripts/content.js
@@ -0,0 +1,25 @@
+let searchListings = document.getElementById("SearchListings"); // listings container
+let priorityListingsDivider = document.getElementById("divPriorityListingsTitle");
+let searchListingsChildren = searchListings.children;
+
+function clearPriorityListings() {
+    priorityListingsDivider.remove();
+
+    if (searchListings) {
+        for (var i = 0; i < searchListingsChildren.length; i++) {
+            if (searchListingsChildren[i].classList.contains("priority-qa") 
+                || searchListingsChildren[i].classList.contains("billboard") 
+                || searchListingsChildren[i].classList.contains("billboardBannerElement")) 
+            {
+                searchListingsChildren[i].outerHTML = `<div class="hidden"></div>`;
+            }
+        }
+    }
+}
+
+const mutationObserver = new MutationObserver(() => {
+    clearPriorityListings();
+})
+
+clearPriorityListings(); // initial clear after page loads
+mutationObserver.observe(searchListings, { childList: true });
